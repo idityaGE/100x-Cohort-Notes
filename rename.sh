bbacc584b59c7937920431a3dfa5e47c3d3840ad
idityaGE <am44910606@gmail.com>
2024-10-03 00:57:12 +0530

    how about that

--- rename.sh
+++ rename.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
-# Get a list of all folders in the current directory
-for folder in Week*; do
+# Get all folders matching the pattern
+for folder in [0-9]*; do
   if [[ -d $folder ]]; then
     # Extract the number from the folder name
-    number=$(echo "$folder" | grep -o -E '[0-9]+')
-
-    # Rename the folder by prepending the number
-    mv "$folder" "${number}- $folder"
+    number=$(echo "$folder" | grep -o -E '^[0-9]+')
+    
+    # Pad the number with leading zeros (up to two digits)
+    new_number=$(printf "%02d" "$number")
+    
+    # Construct the new folder name
+    new_name="${folder/$number/$new_number}"
+    
+    # Rename the folder
+    mv "$folder" "$new_name"
   fi
 done
 
